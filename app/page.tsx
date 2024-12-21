ab652190e8391c5c3bc224d7149b6f70044bc6d8
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2024-12-21 16:22:27 +0500

    jm

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className='bg-slate-900 text-red-200'>Hello world</h1>
+      <br />
+      <h1 className="bg-slate-700 text-slate-300 ">madiha</h1><br />
+      <h1 className='text-slate-200 bg-slate-500 border-solid border-2 border-indigo-600 rounded'>lorems </h1>
+      <button className="p-2 bg-pink-300 m-1 border-yellow-500 border-4 rounded-md">login</button>
+    </div>
+  )
+}
+
+export default Page
